3fb8bb363e278e3925f6305545881414227d8186
Ubuntu <ubuntu@ip-172-31-0-132.us-west-2.compute.internal>
2013-07-17 04:50:42 +0000

    Added Git global information

--- setup.sh
+++ setup.sh
@@ -50,3 +50,5 @@ ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
 ln -sf dotfiles/.emacs.d .
 
+git config --global user.name "Gabriel Barcia"
+git config --global user.email "gabriel.barcia@gmail.com"
